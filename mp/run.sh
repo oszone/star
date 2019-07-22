073241fba60983979589056c151314bb0d889ca9
TX <luoxiao0929@163.com>
2019-07-22 15:37:55 +0800

    .

--- mp/run.sh
+++ mp/run.sh
@@ -1,1 +1,1 @@
-create-dmg StarSSR.app --overwrite && ~/MY/appzone/StarSSR客户端/Pods/Sparkle/bin/generate_appcast ~/MY/oszone/star/mp && rm -r StarSSR.app && git add . && git commit -m"." && git push
+create-dmg StarSSR.app --overwrite && ~/MY/appzone/StarSSR客户端/Pods/Sparkle/bin/generate_appcast ~/MY/oszone/star/mp && cp -f *.dmg ~/MY/oszone/star/dmg/星星加速.dmg && rm -r StarSSR.app && git add . && git commit -m"." && git push
